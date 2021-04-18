f18cd53b00dda7b388bc62ac6eef724bd93758e8
lllliuyuiyuan <49908706+lllliuyuiyuan@users.noreply.github.com>
2021-04-18 10:53:05 +0800

    加入超声波测距和模拟量
    加入adc文件
    在CONFIG.H里面加入adc.h
    test.c:
    加入超声波测距
    加入模拟量
    出现变量名重复，用ShowSpeed数组显示转速，dis[7]改成ShowSpeed[7];
    蜂鸣器改为pc10口输出；

---
+++ HARDWARE/ADC/adc.h
@@ -0,0 +1,25 @@
+#ifndef __ADC_H
+#define __ADC_H	
+
+#define ADC_CH2  2 //ͨ��2
+#define ADC_CH3  3 //ͨ��3	   
+ 
+void Adc_Init(void);
+unsigned int  Get_Adc(unsigned char ch);  
+ 
+#endif 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
